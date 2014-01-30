167fa77a0d345fe27cafef3bb0931580c533d4b5
Maxanier <maxanier@gmx.de>
2014-01-30 16:11:37 +0100

    Added java-doc/comment

--- src/com/sec/client/EncryptedClient.java
+++ src/com/sec/client/EncryptedClient.java
@@ -4,28 +4,41 @@ import com.sec.abi.Client;
 
 public abstract class EncryptedClient extends Client {
 
+	
+	/**
+	 * Creates a new EncryptedClient and establishes a encrypted connection.
+	 * @param pIPAdresse
+	 * @param pPortNr
+	 */
 	public EncryptedClient(String pIPAdresse, int pPortNr) {
 		super(pIPAdresse, pPortNr);
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Decrypts message and passes it to processEncryptedMessage()
+	 */
 	@Override
 	public final void processMessage(String pMessage) {
 		// TODO Auto-generated method stub
 		
 	}
 	
+	/**
+	 * Encrypts the message and sends it to the server
+	 */
 	@Override
 	public final void send(String pMessage){
 		
 	}
 	
+	/**
+	 * Is called when a message is received
+	 * @param pMessage
+	 */
 	public abstract void processEncryptedMessage(String pMessage);
 	
 	
-	//Vemutlich überflüssig
-	public void sendEncrypted(String pMessage){
-		
-	}
+
 
 }
--- src/com/sec/server/EncryptedServer.java
+++ src/com/sec/server/EncryptedServer.java
@@ -9,34 +9,68 @@ public abstract class EncryptedServer extends Server {
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Processes new connection and starts the encrypting process respectively the Diffie-Hellmanm-Key-exchange
+	 */
 	@Override
 	public final void processNewConnection(String pClientIP, int pClientPort){
 		
 	}
 	
+	/**
+	 * Processes new Message, which is either used to establish the encrypted connection or ,if already encrypted, decrypted and calls processEncryptedMessage() 
+	 */
 	@Override
 	public final void processMessage(String pClientIP, int pClientPort, String pMessage){
 		
 	}
 	
+	/**
+	 * Encrypts the message and sends it to the given client
+	 */
 	@Override
 	public final void send(String pClientIP, int pClientPort, String pMessage){
 		
 	}
 	
+	/**
+	 * Encrypts the message and sends it to all clients
+	 * @param pMessage message
+	 */
 	@Override
 	public final void sendToAll(String pMessage){
 		
 	}
 	
+	
+	/**
+	 * Closes connection and ends encryption
+	 */
 	@Override
 	public final void processClosedConnection(String pClientIP, int pClientPort){
 		
 	}
   
-	
+	/**
+	 * Is called if a new encrypted connection is established.
+	 * @param pClientIp ClientIp
+	 * @param pClientPort ClientPort
+	 */
 	public abstract void processNewEncryptedConntection(String pClientIp,int pClientPort);
+	
+	/**
+	 * Is called if the server receives a new encrypted message.
+	 * @param pClientIP ClientIp
+	 * @param pClientPort ClientPort
+	 * @param pMessage Decrypted Message
+	 */
 	public abstract void processEncryptedMessage(String pClientIP, int pClientPort, String pMessage);
+	
+	/**
+	 * Is called if encrypted connection is closed.
+	 * @param pClientIP ClientIp
+	 * @param pClientPort ClientPort
+	 */
 	public abstract void processClosedEncryptedConnection(String pClientIP, int pClientPort);
 	
 
