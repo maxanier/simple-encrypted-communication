661ee49f9b34b5d69d6a1a057c44134dc5bdd1fd
maxanier <maxanier@gmx.de>
2014-01-31 04:35:37 -0300

    Anpassung

--- src/com/sec/util/Protokoll.java
+++ src/com/sec/util/Protokoll.java
@@ -2,8 +2,8 @@ package com.sec.util;
 
 public class Protokoll {
 	
-	public final static String SERVER_START="+START ";//START <p> <q> <A>
-	public final static String CLIENT_START="START2 ";//START2 <B>
+	public final static String SERVER_START="+START";//START <p> <q> <A>
+	public final static String CLIENT_START="START2";//START2 <B>
 	public final static String SERVER_OK="+OK";
 	public final static String MESSAGE="MSG";//MSG <msg>
 }
