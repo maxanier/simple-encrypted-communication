78d74683afb713f23024c2fd9f3c286eb8cde26b
maxanier <maxanier@gmx.de>
2014-01-31 04:33:04 -0300

    Protokoll Klasse

---
+++ src/com/sec/util/Protokoll.java
@@ -0,0 +1,9 @@
+package com.sec.util;
+
+public class Protokoll {
+	
+	public final static String SERVER_START="+START ";//START <p> <q> <A>
+	public final static String CLIENT_START="START2 ";//START2 <B>
+	public final static String SERVER_OK="+OK";
+	public final static String MESSAGE="MSG";//MSG <msg>
+}
