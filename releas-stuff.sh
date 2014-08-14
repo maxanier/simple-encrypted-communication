cb8bb47ed76c93a9029c206a7e5ea55943a53a44
Maxanier <maxanier@gmx.de>
2014-08-14 19:53:40 +0200

    #release test

--- releas-stuff.sh
+++ releas-stuff.sh
@@ -8,19 +8,16 @@ if [[ "${commsg}" != *\#release* ]]; then
     exit
 fi
 
-origin=$(git config --get remote.origin.url)
-origin=${origin%.git}
+origin=https://github.com/${2}/${3}
 echo "Origin url: " $origin
 lasttag=$(git describe --abbrev=0 --tags)
 echo "Last tag: "+lasttag
 changelog=$(git log ${lasttag}..  --pretty=format:'<li> <a href="'${origin}'/commit/%H">view commit </a> &bull; %s</li> ' --reverse )
 changelogfile=build/changelog.html
 echo $changelog > $changelogfile
-
+echo "Version:" $1 
 API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $1 $1 $1)
-owner="maxanier"
-repo="MinecraftSecondScreenMod"
-token="e3a447b30d370af965e0056242175703ea9ce15e"
-curl --data "$API_JSON" https://api.github.com/repos/${owner}/${repo}/releases?access_token=${token}
+token=$(printenv TOKEN)
+curl --data "$API_JSON" https://api.github.com/repos/${2}/${3}/releases?access_token=${token}
 echo $API_JSON
 read -p "wait"
