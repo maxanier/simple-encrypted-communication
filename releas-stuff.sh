03d6b79e5f90a2d7b9e755a0e832afd94e67d207
Maxanier <maxanier@gmx.de>
2014-08-14 19:55:35 +0200

    #release test

--- releas-stuff.sh
+++ releas-stuff.sh
@@ -3,7 +3,7 @@
 commsg=$(git show -s --format=%s $(printenv GIT_COMMIT))
 echo "Commit message: " $commsg
  
-if [[ "${commsg}" != *\#release* ]]; then
+if [test "${commsg}" != *\#release* ]; then
 	echo "Commit does not include \#release"
     exit
 fi
@@ -11,13 +11,12 @@ fi
 origin=https://github.com/${2}/${3}
 echo "Origin url: " $origin
 lasttag=$(git describe --abbrev=0 --tags)
-echo "Last tag: "+lasttag
+echo "Last tag: " $lasttag
 changelog=$(git log ${lasttag}..  --pretty=format:'<li> <a href="'${origin}'/commit/%H">view commit </a> &bull; %s</li> ' --reverse )
-changelogfile=build/changelog.html
+changelogfile=changelog.html
 echo $changelog > $changelogfile
 echo "Version:" $1 
 API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $1 $1 $1)
 token=$(printenv TOKEN)
 curl --data "$API_JSON" https://api.github.com/repos/${2}/${3}/releases?access_token=${token}
 echo $API_JSON
-read -p "wait"
