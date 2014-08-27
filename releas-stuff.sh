5f28b9cc07af2a911e31d0bef924b12a6592806f
Maxanier <maxanier@gmx.de>
2014-08-27 18:48:25 +0200

    #release #changelog should work now

--- releas-stuff.sh
+++ releas-stuff.sh
@@ -47,7 +47,7 @@ chpass=$(printenv PASS)
 if [ $3 ]
 then
 	echo "Uploading changelog"
-	result=$(curl --data "major=${major},minor=${minor},change=${changelog},pass=${pass},build=$(printenv DRONE_BUILD_NUMBER)" ${3})
+	result=$(curl --data "major=${major},minor=${minor},change=${changelog},pass=${chpass},build=$(printenv DRONE_BUILD_NUMBER)" ${3})
 	echo "Result: "$result
 fi
 
